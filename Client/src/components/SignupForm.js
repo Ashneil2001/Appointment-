064a8747ef10589dbc52921b3f7bf5b0f02de793
Ashneil2001 <ashnil220@gmail.com>
2022-06-29 13:50:56 +0530

    add files

---
+++ Client/src/components/SignupForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './SignupForm.css';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+export default function SignupForm() {
+
+
+  const [data,setdata] = useState({
+    email:'',
+    pass:'',
+    confirmpass:''
+  });
+
+  const setemail = (e)=>{
+    setdata({
+      email:e.target.value,
+      pass:data.pass,
+      confirmpass:data.confirmpass
+    })
+  }
+  const setpass = (e)=>{
+    setdata({
+      email:data.email,
+      pass:e.target.value,
+      confirmpass:data.confirmpass
+    })
+  }
+  const setconfirm = (e)=>{
+    setdata({
+      email:data.email,
+      pass:data.pass,
+      confirmpass:e.target.value
+    })
+    console.log(data)
+  }
+  const navigate = useNavigate();
+
+  const submitdata =()=>{
+
+    console.log(data);
+    axios.post('http://localhost:5000/signup',data).then((res)=>{if(res.status===200)navigate('/')});
+
+  }
+
+
+  return (
+    <div className='sign'>
+
+        <div className="header">Sign Up</div>
+        <div className="inp">
+            <input type="email" name='email' placeholder='Email-Id' className='text' value={data.email} onChange={setemail}/>
+            <input type="password"  name='pass' placeholder='Password' className='text' value={data.pass} onChange={setpass}/>
+            <input type="password" name="confirmpass" placeholder='Confirm Password' className='text' value={data.confirmpass} onChange={setconfirm}/>
+        </div>
+        <button className='btn' onClick={submitdata}>Submit</button>
+      
+    </div>
+  )
+}
